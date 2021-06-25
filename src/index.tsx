293875f37526052826dd7bb3c3c989d51679a59c
Hyun Oh <hoh@mondrian.ai>
2021-06-25 13:30:33 +0900

    Add theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
+
 import "./index.css";
+
 import App from "./infrastructure/ui/App";
 import reportWebVitals from "./reportWebVitals";
+import theme from "./infrastructure/ui/theme.json";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/infrastructure/ui/features/working-area/WorkingArea.tsx
+++ src/infrastructure/ui/features/working-area/WorkingArea.tsx
@@ -1,3 +1,15 @@
+import styled, { css } from "styled-components";
+
+const WorkspaceBackground = styled.div`
+  height: 100%;
+  ${(props) => {
+    const backgroundColor = props.theme.background;
+    return css`
+      background-color: ${backgroundColor};
+    `;
+  }}
+`;
+
 export default function WorkingArea() {
-  return <section></section>;
+  return <WorkspaceBackground />;
 }
