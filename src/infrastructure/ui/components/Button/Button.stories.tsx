be1445e2bbf67af411070058ccc52d38659f7864
HyunnoH <hyunnoh01@gmail.com>
2021-06-26 00:31:56 +0900

    Make hover event changes activation state

--- src/infrastructure/ui/components/Button/Button.stories.tsx
+++ src/infrastructure/ui/components/Button/Button.stories.tsx
@@ -1,15 +1,28 @@
-import { text, withKnobs } from "@storybook/addon-knobs";
+import { select, text, withKnobs } from "@storybook/addon-knobs";
 import { themeDecorator } from "../../styles/withTheme";
 import Button from "./Button";
 
 export default {
-  title: "Button",
+  title: "Components/Button",
   component: Button,
   decorators: [withKnobs, themeDecorator],
 };
 
 export const Default = () => {
   const children = text("children", "Click this");
+  const active = select(
+    "active",
+    {
+      true: true,
+      false: false,
+      auto: undefined,
+    },
+    undefined
+  );
 
-  return <Button>{children}</Button>;
+  return (
+    <Button active={typeof active === "boolean" ? active : undefined}>
+      {children}
+    </Button>
+  );
 };
--- src/infrastructure/ui/components/Button/Button.tsx
+++ src/infrastructure/ui/components/Button/Button.tsx
@@ -1,3 +1,5 @@
+import { MouseEvent } from "react";
+import { MouseEventHandler, useCallback, useState } from "react";
 import styled, { css } from "styled-components";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -21,8 +23,46 @@ const StyledButton = styled.button<ButtonProps>`
   }}
 `;
 
-function Button({ children, selected, ...restProps }: ButtonProps) {
-  return <StyledButton {...restProps}>{children}</StyledButton>;
+function Button({
+  active,
+  children,
+  selected,
+  onMouseOver,
+  onMouseOut,
+  ...restProps
+}: ButtonProps) {
+  const [hoverActive, setHoverActive] = useState(false);
+
+  const handleMouseOver = useCallback(
+    (e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
+      setHoverActive(true);
+      if (onMouseOver) {
+        onMouseOver(e);
+      }
+    },
+    [onMouseOver]
+  );
+
+  const handleMouseOut = useCallback(
+    (e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
+      setHoverActive(false);
+      if (onMouseOut) {
+        onMouseOut(e);
+      }
+    },
+    [onMouseOut]
+  );
+
+  return (
+    <StyledButton
+      active={active ?? hoverActive}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+      {...restProps}
+    >
+      {children}
+    </StyledButton>
+  );
 }
 
 export default Button;
