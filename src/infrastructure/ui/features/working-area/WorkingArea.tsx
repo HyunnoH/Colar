eb158017107e6ccade54627909ed8f9dc6768f7b
HyunnoH <hyunnoh01@gmail.com>
2021-06-28 18:45:06 +0900

    Add canvas actions

--- src/infrastructure/ui/features/working-area/WorkingArea.tsx
+++ src/infrastructure/ui/features/working-area/WorkingArea.tsx
@@ -1,6 +1,10 @@
+import React, { useCallback, useRef } from "react";
 import styled, { css } from "styled-components";
 
 const WorkspaceBackground = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   flex: 1;
   height: calc(100vh - 68px);
   ${(props) => {
@@ -9,8 +13,59 @@ const WorkspaceBackground = styled.div`
       background-color: ${backgroundColor};
     `;
   }}
+
+  > canvas {
+    width: 60%;
+    height: 60%;
+    overflow-x: scroll;
+    background-color: white;
+  }
 `;
 
+type Position2D = {
+  x: number;
+  y: number;
+};
+
 export default function WorkingArea() {
-  return <WorkspaceBackground />;
+  const isDrawing = useRef(false);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const handleMouseMove = useCallback(
+    (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+      const canvas = canvasRef.current;
+
+      if (!canvas || !isDrawing.current) {
+        return;
+      }
+
+      const currentPosition: Position2D = {
+        x: e.clientX - canvas.offsetLeft,
+        y: e.clientY - canvas.offsetTop,
+      };
+
+      const ctx = canvas.getContext("2d");
+
+      ctx?.beginPath();
+      ctx?.moveTo(currentPosition.x, currentPosition.y);
+    },
+    []
+  );
+
+  return (
+    <WorkspaceBackground>
+      <canvas
+        ref={canvasRef}
+        onMouseMove={handleMouseMove}
+        onMouseUp={() => {
+          isDrawing.current = false;
+        }}
+        onMouseDown={(e) => {
+          if (e.button === 0) {
+            isDrawing.current = true;
+          }
+        }}
+      />
+    </WorkspaceBackground>
+  );
 }
