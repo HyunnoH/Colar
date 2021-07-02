dab552b857fc60fe7845db82c70251f4d14ae0a6
HyunnoH <hyunnoh01@gmail.com>
2021-07-02 17:29:26 +0900

    Implement basic canvas drawing

--- src/infrastructure/ui/features/working-area/WorkingArea.tsx
+++ src/infrastructure/ui/features/working-area/WorkingArea.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useRef } from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import styled, { css } from "styled-components";
 import { useToolbarItem } from "../../../stores/modules/toolbar/hooks";
@@ -16,12 +17,15 @@ const WorkspaceBackground = styled.div`
     `;
   }}
 
-  > canvas {
-    width: 60%;
-    height: 60%;
+  canvas {
     overflow-x: scroll;
     background-color: white;
   }
+`;
+
+const CanvasWrapper = styled.div`
+  width: 60%;
+  height: 60%;
 `;
 
 type Position2D = {
@@ -29,50 +33,127 @@ type Position2D = {
   y: number;
 };
 
+type Dimension2D = {
+  width: number;
+  height: number;
+};
+
 export default function WorkingArea() {
   const isDrawing = useRef(false);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const previousPos = useRef<Position2D>({
+    x: 0,
+    y: 0,
+  });
+  const currentPos = useRef<Position2D>({
+    x: 0,
+    y: 0,
+  });
   const mod = useToolbarItem();
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const [wrapperSize, setWrapperSize] = useState<Partial<Dimension2D>>({
+    width: 100,
+    height: 100,
+  });
 
   useEffect(() => {
-    console.log(mod);
-  }, [mod]);
+    setWrapperSize({
+      width: wrapperRef.current?.clientWidth,
+      height: wrapperRef.current?.clientHeight,
+    });
+  }, []);
 
-  const handleMouseMove = useCallback(
-    (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
-      const canvas = canvasRef.current;
-
-      if (!canvas || !isDrawing.current) {
-        return;
-      }
-
-      const currentPosition: Position2D = {
+  const changePosition = useCallback(
+    (
+      canvas: HTMLCanvasElement,
+      e: React.MouseEvent<HTMLCanvasElement, MouseEvent>
+    ) => {
+      previousPos.current = currentPos.current;
+      currentPos.current = {
         x: e.clientX - canvas.offsetLeft,
         y: e.clientY - canvas.offsetTop,
       };
 
-      const ctx = canvas.getContext("2d");
+      console.log(
+        "mousemove:",
+        "previouse:",
+        previousPos.current,
+        "current:",
+        currentPos.current
+      );
+    },
+    []
+  );
 
-      ctx?.beginPath();
-      ctx?.moveTo(currentPosition.x, currentPosition.y);
+  const handleMouseMove = useCallback(
+    (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+      const canvas = canvasRef.current;
+      const ctx = canvas?.getContext("2d");
+      if (!canvas || !isDrawing.current || !ctx) {
+        return;
+      }
+
+      changePosition(canvas, e);
+
+      const fillStyle = "black";
+
+      if (mod === "brush") {
+        ctx.beginPath();
+        ctx.moveTo(previousPos.current.x, previousPos.current.y);
+        ctx.lineTo(currentPos.current.x, currentPos.current.y);
+        ctx.globalCompositeOperation = "source-over";
+        ctx.strokeStyle = fillStyle;
+        ctx.lineWidth = 1;
+        ctx.stroke();
+        ctx.closePath();
+      } else if (mod === "eraser") {
+        ctx.beginPath();
+        ctx.moveTo(previousPos.current.x, previousPos.current.y);
+        ctx.lineTo(currentPos.current.x, currentPos.current.y);
+        ctx.globalCompositeOperation = "destination-out";
+        ctx.strokeStyle = fillStyle;
+        ctx.lineWidth = 1;
+        ctx.stroke();
+        ctx.closePath();
+      }
+    },
+    [mod]
+  );
+
+  const handleMouseDown = useCallback(
+    (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+      const canvas = canvasRef.current;
+      const ctx = canvas?.getContext("2d");
+      if (e.button !== 0 || !canvas || !ctx) {
+        return;
+      }
+
+      isDrawing.current = true;
+
+      changePosition(canvas, e);
+
+      ctx.beginPath();
+      ctx.fillStyle = "black";
+      ctx.fillRect(currentPos.current.x, currentPos.current.y, 2, 2);
+      ctx.closePath();
     },
     []
   );
 
   return (
     <WorkspaceBackground>
-      <canvas
-        ref={canvasRef}
-        onMouseMove={handleMouseMove}
-        onMouseUp={() => {
-          isDrawing.current = false;
-        }}
-        onMouseDown={(e) => {
-          if (e.button === 0) {
-            isDrawing.current = true;
-          }
-        }}
-      />
+      <CanvasWrapper ref={wrapperRef}>
+        <canvas
+          ref={canvasRef}
+          width={wrapperSize.width}
+          height={wrapperSize.height}
+          onMouseMove={handleMouseMove}
+          onMouseUp={() => {
+            isDrawing.current = false;
+          }}
+          onMouseDown={handleMouseDown}
+        />
+      </CanvasWrapper>
     </WorkspaceBackground>
   );
 }
